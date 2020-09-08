afdb01d418fa2bbcc85e7db84ffd6a9e8bc02346
qingxue0606 <77055664@qq.com>
2020-09-08 17:54:46 +0800

    11
    Signed-off-by: qingxue0606 <77055664@qq.com>

--- src/main/java/com/zhuozhengsoft/samples5/controller/Controller.java
+++ src/main/java/com/zhuozhengsoft/samples5/controller/Controller.java
@@ -1,6 +1,4 @@
 package com.zhuozhengsoft.samples5.controller;
-
-
 import com.zhuozhengsoft.pageoffice.FileSaver;
 import com.zhuozhengsoft.pageoffice.OpenModeType;
 import com.zhuozhengsoft.pageoffice.PageOfficeCtrl;
@@ -14,46 +12,30 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.FileNotFoundException;
 import java.util.Map;
-
 @RestController
-@RequestMapping(value="/SimpleWord/")
-public class SimpleWordController {
-
+@RequestMapping(value="/QQ/")
+public class Controller {
     private String dir= ResourceUtils.getURL("classpath:").getPath()+"static\\doc\\";
-    public SimpleWordController() throws FileNotFoundException {
+    public Controller() throws FileNotFoundException {
     }
     @RequestMapping(value="Word", method= RequestMethod.GET)
     public ModelAndView showWord(HttpServletRequest request, Map<String,Object> map){
         PageOfficeCtrl poCtrl=new PageOfficeCtrl(request);
         poCtrl.setServerPage(request.getContextPath()+"/poserver.zz");//设置服务页面
-        //添加自定义按钮
-        poCtrl.addCustomToolButton("保存","Save",1);
-        poCtrl.addCustomToolButton("打印设置","PrintSet",0);
-        poCtrl.addCustomToolButton("打印","PrintFile",6);
-        poCtrl.addCustomToolButton("全屏/还原", "IsFullScreen", 4);
-        poCtrl.addCustomToolButton("-", "", 0);
-        poCtrl.addCustomToolButton("关闭","Close",21);
-        //设置保存页面
-        poCtrl.setSaveFilePage("save");//设置处理文件保存的请求方法
-        //打开Word文档
-        poCtrl.webOpen("/doc/SimpleWord/test.doc", OpenModeType.docNormalEdit,"张三");
-        map.put("pageoffice",poCtrl.getHtmlCode("PageOfficeCtrl1"));
-        ModelAndView mv = new ModelAndView("SimpleWord/Word");
-        return mv;
-    }
 
-    @RequestMapping(value="Word1", method= RequestMethod.GET)
-    public ModelAndView showWord2(HttpServletRequest request, Map<String,Object> map){
-        PageOfficeCtrl poCtrl=new PageOfficeCtrl(request);
-        poCtrl.setServerPage(request.getContextPath()+"/poserver.zz");//设置服务页面
+
         //添加自定义按钮
         poCtrl.addCustomToolButton("保存","Save",1);
+
+
         //设置保存页面
         poCtrl.setSaveFilePage("save");//设置处理文件保存的请求方法
+
+
         //打开Word文档
-        poCtrl.webOpen("/doc/SimpleWord/test.doc", OpenModeType.docNormalEdit,"张三");
+        poCtrl.webOpen("/doc/QQ/test.doc", OpenModeType.docNormalEdit,"张三");
         map.put("pageoffice",poCtrl.getHtmlCode("PageOfficeCtrl1"));
-        ModelAndView mv = new ModelAndView("SimpleWord/Word1");
+        ModelAndView mv = new ModelAndView("QQ/Word");
         return mv;
     }
 
@@ -61,7 +43,7 @@ public class SimpleWordController {
     @RequestMapping("save")
     public void save(HttpServletRequest request, HttpServletResponse response){
         FileSaver fs = new FileSaver(request, response);
-        fs.saveToFile(dir+ "SimpleWord\\"+fs.getFileName());
+        fs.saveToFile(dir+ "QQ\\"+fs.getFileName());
         fs.close();
     }
 
---
+++ src/main/java/com/zhuozhengsoft/samples5/controller/SetThemeController.java
@@ -0,0 +1,37 @@
+package com.zhuozhengsoft.samples5.controller;
+import com.zhuozhengsoft.pageoffice.FileSaver;
+import com.zhuozhengsoft.pageoffice.OpenModeType;
+import com.zhuozhengsoft.pageoffice.PageOfficeCtrl;
+import com.zhuozhengsoft.pageoffice.ThemeType;
+import org.springframework.util.ResourceUtils;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.FileNotFoundException;
+import java.util.Map;
+
+@RestController
+@RequestMapping(value="/SetTheme/")
+public class SetThemeController {
+
+    @RequestMapping(value="Word", method= RequestMethod.GET)
+    public ModelAndView showWord(HttpServletRequest request, Map<String,Object> map){
+        PageOfficeCtrl poCtrl=new PageOfficeCtrl(request);
+        poCtrl.setServerPage(request.getContextPath()+"/poserver.zz");//设置服务页面
+        //设置打开的Word文档的主题
+        poCtrl.setTheme(ThemeType.Office2010);
+
+        //设置保存页面
+        poCtrl.setSaveFilePage("save");//设置处理文件保存的请求方法
+        //打开Word文档
+        poCtrl.webOpen("/doc/SetTheme/test.doc", OpenModeType.docNormalEdit,"张三");
+        map.put("pageoffice",poCtrl.getHtmlCode("PageOfficeCtrl1"));
+        ModelAndView mv = new ModelAndView("SetTheme/Word");
+        return mv;
+    }
+
+}
