6336b86610497a69c831f0bfe943554d388e1de9
qingxue0606 <77055664@qq.com>
2020-09-08 14:41:57 +0800

    11
    Signed-off-by: qingxue0606 <77055664@qq.com>

--- src/main/java/com/zhuozhengsoft/samples5/controller/SimplePPTController.java
+++ src/main/java/com/zhuozhengsoft/samples5/controller/SimplePPTController.java
@@ -4,7 +4,6 @@ package com.zhuozhengsoft.samples5.controller;
 import com.zhuozhengsoft.pageoffice.FileSaver;
 import com.zhuozhengsoft.pageoffice.OpenModeType;
 import com.zhuozhengsoft.pageoffice.PageOfficeCtrl;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.util.ResourceUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -17,52 +16,34 @@ import java.io.FileNotFoundException;
 import java.util.Map;
 
 @RestController
-@RequestMapping(value="/SimpleWord/")
-public class SimpleWordController {
+@RequestMapping(value="/SimplePPT/")
+public class SimplePPTController {
 
     private String dir= ResourceUtils.getURL("classpath:").getPath()+"static\\doc\\";
-    public SimpleWordController() throws FileNotFoundException {
+    public SimplePPTController() throws FileNotFoundException {
     }
-    @RequestMapping(value="Word", method= RequestMethod.GET)
+    @RequestMapping(value="PPT", method= RequestMethod.GET)
     public ModelAndView showWord(HttpServletRequest request, Map<String,Object> map){
         PageOfficeCtrl poCtrl=new PageOfficeCtrl(request);
         poCtrl.setServerPage(request.getContextPath()+"/poserver.zz");//设置服务页面
         //添加自定义按钮
         poCtrl.addCustomToolButton("保存","Save",1);
-        poCtrl.addCustomToolButton("打印设置","PrintSet",0);
-        poCtrl.addCustomToolButton("打印","PrintFile",6);
-        poCtrl.addCustomToolButton("全屏/还原", "IsFullScreen", 4);
-        poCtrl.addCustomToolButton("-", "", 0);
         poCtrl.addCustomToolButton("关闭","Close",21);
         //设置保存页面
         poCtrl.setSaveFilePage("save");//设置处理文件保存的请求方法
         //打开Word文档
-        poCtrl.webOpen("/doc/SimpleWord/test.doc", OpenModeType.docNormalEdit,"张三");
+        poCtrl.webOpen("/doc/SimplePPT/test.ppt", OpenModeType.pptNormalEdit,"张三");
         map.put("pageoffice",poCtrl.getHtmlCode("PageOfficeCtrl1"));
-        ModelAndView mv = new ModelAndView("SimpleWord/Word");
+        ModelAndView mv = new ModelAndView("SimplePPT/PPT");
         return mv;
     }
 
-    @RequestMapping(value="Word1", method= RequestMethod.GET)
-    public ModelAndView showWord2(HttpServletRequest request, Map<String,Object> map){
-        PageOfficeCtrl poCtrl=new PageOfficeCtrl(request);
-        poCtrl.setServerPage(request.getContextPath()+"/poserver.zz");//设置服务页面
-        //添加自定义按钮
-        poCtrl.addCustomToolButton("保存","Save",1);
-        //设置保存页面
-        poCtrl.setSaveFilePage("save");//设置处理文件保存的请求方法
-        //打开Word文档
-        poCtrl.webOpen("/doc/SimpleWord/test.doc", OpenModeType.docNormalEdit,"张三");
-        map.put("pageoffice",poCtrl.getHtmlCode("PageOfficeCtrl1"));
-        ModelAndView mv = new ModelAndView("SimpleWord/Word1");
-        return mv;
-    }
 
 
     @RequestMapping("save")
     public void save(HttpServletRequest request, HttpServletResponse response){
         FileSaver fs = new FileSaver(request, response);
-        fs.saveToFile(dir+ "SimpleWord\\"+fs.getFileName());
+        fs.saveToFile(dir+ "SimplePPT\\"+fs.getFileName());
         fs.close();
     }
 
